c46a7dfeccb53f8167006b5ead45d130c43ad3ea
fernando.chagas <fermaccha@gmail.com>
2018-08-14 15:32:23 -0400

    Terminado modulo de autenticação customizado com Spring Security

--- src/main/java/br/com/primemacedo/comercial/security/Seguranca.java
+++ src/main/java/br/com/primemacedo/comercial/security/Seguranca.java
@@ -1,7 +1,9 @@
 package br.com.primemacedo.comercial.security;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +11,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 @Named
 @RequestScoped
 public class Seguranca {
+	
+	private @Inject ExternalContext externalContext;
+	
+	
 	
 	public String getNomeUsuario() {
 		String nome = null;
@@ -33,4 +39,13 @@ public class Seguranca {
 		
 		return usuario;
 	}
+	
+	public boolean isEmitirPedidoPermitido() {
+		return externalContext.isUserInRole("ADMINISTRADORES") || externalContext.isUserInRole("VENDEDORES");
+	}
+	
+	public boolean isCancelarPedidoPermitido() {
+		return externalContext.isUserInRole("ADMINISTRADORES") || externalContext.isUserInRole("VENDEDORES");
+	}
+	
 }
